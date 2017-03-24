4d9ee249386bfa30059cf72cd293ef83905dc07e
li <huguohuili@gmail.com>
2017-03-24 20:59:46 +0800

    README更新

--- src/main/java/cn/godbol/common/aop/LoggingAspect.java
+++ src/main/java/cn/godbol/common/aop/LoggingAspect.java
@@ -39,14 +39,6 @@ public class LoggingAspect {
      */
     @AfterThrowing(pointcut = "loggingPointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-//        if (environment.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)) {
-//            log.error("Exception in {}.{}() with cause = \'{}\' and exception = \'{}\'", joinPoint.getSignature().getDeclaringTypeName(),
-//                    joinPoint.getSignature().getName(), e.getCause() != null? e.getCause() : "NULL", e.getMessage(), e);
-//
-//        } else {
-//            log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-//                    joinPoint.getSignature().getName(), e.getCause() != null? e.getCause() : "NULL");
-//        }
     }
 
     /**
--- src/main/java/cn/godbol/config/LoggingAspectConfig.java
+++ src/main/java/cn/godbol/config/LoggingAspectConfig.java
@@ -1,7 +1,12 @@
 package cn.godbol.config;
 
+import cn.godbol.common.aop.FindAspect;
+import cn.godbol.common.aop.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by li on 17-3-12.
@@ -9,4 +14,13 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 public class LoggingAspectConfig {
+    @Bean
+    public FindAspect findAspect(){
+        return new FindAspect();
+    }
+
+    @Bean
+    public LoggingAspect loggingAspect(Environment environment){
+        return new LoggingAspect(environment);
+    }
 }
