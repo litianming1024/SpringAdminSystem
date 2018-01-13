48c5753839aaeb30e9f9b2d6dd0b0cf32b6a47eb
litianming1024 <litianming1024@gmail.com>
2018-01-14 00:02:31 +0800

    更新一堆，未完全测试

---
+++ springqs-domain/src/main/java/cn/godbol/domain/Authority.java
@@ -0,0 +1,41 @@
+package cn.godbol.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+
+/**
+ * Created by Li on 2016/10/14.
+ */
+@Getter @Setter
+@Entity
+@Table(name = "tb_authority")
+public class Authority extends BaseModel {
+
+    @Column(length = 50)
+    private String c_name;
+
+    @Column(length = 50)
+    private String e_name;
+    private Long parent_id;
+    private boolean has_child;
+    private String method;
+
+    @Column(length = 50)
+    private String url;
+
+    @Column(length = 50)
+    private String icon;
+
+    private Long sort;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Collection<Group> groups;
+
+
+}
---
+++ springqs-domain/src/main/java/cn/godbol/domain/Group.java
@@ -0,0 +1,34 @@
+package cn.godbol.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * Created by Li on 2016/10/14.
+ */
+@Getter @Setter
+@Entity
+@Table(name = "tb_group")
+public class Group extends BaseModel {
+
+    @Column(length = 50)
+    private String e_name;
+    @Column(length = 50)
+    private String c_name;
+    //This is for father group and child group.
+    private Long parent_id;
+    private boolean has_child;
+
+    @ManyToMany(mappedBy = "groups")
+    private Collection<User> users;
+
+    @ManyToMany
+    @JoinTable(name = "tb_groups_authorities",
+            joinColumns = @JoinColumn(name = "group_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private Collection<Authority> authorities;
+
+}
